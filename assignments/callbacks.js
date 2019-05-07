71e696b41a9c89150d50a94d9f8734a082d079b7
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-05-07 16:03:48 +0100

    done most of callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -43,16 +43,36 @@ last(items, (index) => {
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(2, 3, (callBack) => {
+  console.log(callBack);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(2, 3, (callBack) => {
+  console.log(callBack);
+})
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+
+// function contains (item, list, cb) {
+//   for (let i = 0; i < list.length, i++);
+//   if (item === list[i]) {
+//     return cb(list [i]);
+//     }
+// }
+
+
+// contains('Gum', items, (callBack) => {
+// console.log(callBack);
+// });
 
 /* STRETCH PROBLEM */
 
